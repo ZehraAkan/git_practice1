89291c949aa0b9c435a603ee151cc6db14da71d9
hh <zehraakan1998@gmail.com>
2022-11-06 21:58:21 +0300

    Nihal

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Harry Potter");
+
+        System.out.println("Hermione Granger");
     }
 
     // Doing some work here (you can type anything you want)
